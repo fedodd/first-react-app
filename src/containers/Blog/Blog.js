c11483ba96fcbd264001d2222c7e140047f30f15
fedodd <fedodd.s@gmail.com>
2019-07-18 10:48:17 +0300

    routing last changes before change branch

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+//import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
 
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
+
 class Blog extends Component {
     state = {
-        auth: false,
+        auth: true,
     }
-
-
 
     render () {
         
@@ -39,7 +42,7 @@ class Blog extends Component {
                 </header>
                 
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found.</h1>}/>
                     {/* <Route path="/posts" component={Posts} />
