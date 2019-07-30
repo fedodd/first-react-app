e01e9ef024bfd923dc0f804e1ecec7ae12820922
fedodd <fedodd.s@gmail.com>
2019-07-30 13:15:34 +0300

    finished redux basic

---
+++ old/src/containers/Counter/Counter.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionTypes from '../../store/actions';
+
+class Counter extends Component {
+    state = {
+        counter: 0,
+        results: []
+    }
+
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubscractCounter}  />
+                <hr />
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+                <ul>
+                    {this.props.storeResults.map(strResult => (
+                        <li 
+                            onClick={() => this.props.onDeleteResult(strResult.id)}
+                            key={strResult.id}>{strResult.value}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.ctr.counter,
+        storeResults: state.res.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({
+            type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({
+            type: actionTypes.DECREMENT }),
+        onAddCounter: () => dispatch({
+            type: actionTypes.ADD, val: 5 }),
+        onSubscractCounter: () => dispatch({
+            type: actionTypes.SUBTRACT, val: 5 }),
+        onStoreResult: (result) => dispatch({
+            type: actionTypes.STORE_RESULT, result: result}),
+        onDeleteResult: (id) => dispatch({
+            type: actionTypes.DELETE_RESULT, resultElId: id })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,14 +1,28 @@
-const initialState = {
-  counter: 0
-}
+import * as actionTypes from './actions';
 
-const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      counter: state.counter + 1
+const initialState = {
+    persons: []
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case actionTypes.ADD_PERSON:
+            const newPerson = {
+                id: Math.random(), // not really unique but good enough here!
+                name: action.personData.name,
+                age: action.personData.age
+            }
+            return {
+                ...state,
+                persons: state.persons.concat( newPerson )
+            }
+        case actionTypes.REMOVE_PERSON:
+            return {
+                ...state,
+                persons: state.persons.filter(person => person.id !== action.personId)
+            }
     }
-  }
-  return state;
-}
+    return state;
+};
 
 export default reducer;
