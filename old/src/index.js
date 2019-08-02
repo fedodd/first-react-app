e92b1b58500866defb63f2defca2c1c6038f49bb
fedodd <fedodd.s@gmail.com>
2019-08-02 14:13:17 +0300

    advanced add redux thunk and set timeout in middleware

--- old/src/index.js
+++ old/src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import {createStore, combineReducers} from 'redux';
-
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import counterReducer from './store/reducers/counter';
-import resultReducer from './store/reducers/results';
+import reducer from './store/reducer';
 
-const rootReducer = combineReducers({
-  ctr: counterReducer,
-  res: resultReducer 
-});
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middlware] Dispatching',  action);
+      const result = next(action);
+      console.log('[Middlware] next State', store.getState());
+      return result;
+    }
+  }
+}
 
-const store = createStore(rootReducer );
-
+const store = createStore(reducer, null, applyMiddleware);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
