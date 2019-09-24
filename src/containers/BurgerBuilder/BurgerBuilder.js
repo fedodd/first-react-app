1fed23d3483750fe6b08ccfb4eb7ea7db253c448
fedodd <fedodd.s@gmail.com>
2019-09-24 10:56:22 +0300

    auth redirections

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,7 +34,12 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing: true});
+    if (this.props.isAuthenticated) {
+      this.setState({purchasing: true});
+    } else {
+      this.props.history.push('/auth')
+    }
+    
   }
 
   purchaseCancelHandler = () => {
@@ -67,7 +72,8 @@ class BurgerBuilder extends Component {
             disabled={disabledInfo}
             price={this.props.price}
             purchasable={this.updatePurchaseState(this.props.ings)}
-            ordered={this.purchaseHandler} />
+            ordered={this.purchaseHandler}
+            isAuth={this.props.isAuthenticated} />
         </Aux>
       );
       orderSummary = <OrderSummary
@@ -98,7 +104,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   }
 }
 const mapDispatchToProps = dispatch => {
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import { connect } from "react-redux";
+
 import Aux from '../Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -24,9 +26,11 @@ class Layout extends Component {
   render () {
     return (
       <Aux>
-        <Toolbar 
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
           clicked={this.sideDrawerToggleHandler}/>
         <SideDrawer 
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer} 
           closed={this.SideDrawerClosedHandler}/>
         <main className={classes.Content}>
@@ -37,5 +41,12 @@ class Layout extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
 
-export default Layout;
+
+
+export default connect(mapStateToProps)(Layout);
